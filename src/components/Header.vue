f7912a5f8671676b92e17f5164c799f3580ed93b
yaquelinehoyos <yaqueline150@gmail.com>
2021-09-06 14:51:08 -0500

    :sparkles: header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="header-pokemon">
-        <h1>HEADER</h1>
+        <h1 class="header-pokemon__title"><strong>POKÉMON</strong></h1>
     </div>
 </template>
 
@@ -9,3 +9,23 @@ export default {
     name: "HeaderPokemon"
 }
 </script>
+
+<style lang="scss">
+.header-pokemon {
+    min-height: 30vh;
+    background-image: url("../assets/images/header-img.jpg");
+    background-position: center;
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    background-size: cover;
+    box-shadow: inset 0 0 0 1000px rgba($color-dark-gray, 0.6);
+
+    &__title {
+        margin: 0px;
+        padding: 20px;
+        font-size: 50px;
+        text-align: left;
+        color: yellow;
+    }
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,21 @@ const routes = [
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/Home/Index.vue"),
   },
+  {
+    path: "/",
+    name: "Character",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Character/Index.vue"),
+  },
+  {
+    path: "/404",
+    name: "NotFound",
+    component: () => import(`@/views/NotFound/Index.vue`)
+  },
+  {
+    path: "*",
+    redirect: "/404"
+  }
 ];
 
 const router = new VueRouter({
